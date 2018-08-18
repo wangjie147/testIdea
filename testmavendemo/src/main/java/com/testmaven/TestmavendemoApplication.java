a1a73b3a306912111959baca024521678723d589
WILLing <wangle1988918@163.com>
2018-08-18 15:28:00 +0800

    maven测试

---
+++ testmavendemo/src/main/java/com/testmaven/TestmavendemoApplication.java
@@ -0,0 +1,12 @@
+package com.testmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmavendemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestmavendemoApplication.class, args);
+    }
+}
